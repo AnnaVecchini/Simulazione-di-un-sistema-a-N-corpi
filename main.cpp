23133d6800ca70b62adc23aff03550ad51834cd9
tonz-hub <paolo.scorzoni@studio.unibo.it>
2026-08-28 21:49:37 +0200

    blocco try-cacth

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <array>
+#include <exception>
 #include <iostream>
 
 #include "n_bodies.hpp"
@@ -62,42 +63,51 @@ void handle_events(sf::RenderWindow &window, int &steps_per_frame,
 }
 
 int main() {
-  std::vector<pf::Body> bodies =
-      pf::load_bodies_from_file("initial_conditions.txt");
+  try {
+    std::vector<pf::Body> bodies =
+        pf::load_bodies_from_file("initial_conditions.txt");
 
-  pf::SystemTotals initial = pf::compute_totals(bodies);
-  std::cout << "INITIAL TOTAL ENERGY: E(0) = " << initial.E << '\n'
-            << "INITIAL TOTAL MOMENTUM: P(0) = (" << initial.P.x << ", "
-            << initial.P.y << ")" << '\n'
-            << "INITIAL TOTAL ANGULAR MOMENTUM: L(0) = " << initial.L << "\n\n";
+    pf::SystemTotals initial = pf::compute_totals(bodies);
+    std::cout << "INITIAL TOTAL ENERGY: E(0) = " << initial.E << '\n'
+              << "INITIAL TOTAL MOMENTUM: P(0) = (" << initial.P.x << ", "
+              << initial.P.y << ")" << '\n'
+              << "INITIAL TOTAL ANGULAR MOMENTUM: L(0) = " << initial.L
+              << "\n\n";
 
-  pf::Simulation sim(std::move(bodies));
+    pf::Simulation sim(std::move(bodies));
 
-  // create the window
-  sf::RenderWindow window(sf::VideoMode(800, 600), "n_bodies simulation");
-  window.setFramerateLimit(60);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "n_bodies simulation");
+    window.setFramerateLimit(60);
 
-  int SPF{10};  // it stands for steps_per_frame
+    int SPF{10};  // it stands for steps_per_frame
 
-  int step{};
+    int step{};
 
-  float scale{200.f};
+    float scale{200.f};
 
-  while (window.isOpen()) {
-    handle_events(window, SPF, scale);
+    while (window.isOpen()) {
+      handle_events(window, SPF, scale);
 
-    window.clear();
-    draw_bodies(sim.get_bodies(), window, scale);
-    window.display();
+      window.clear();
+      draw_bodies(sim.get_bodies(), window, scale);
+      window.display();
 
-    for (int i{}; i < SPF; ++i) {
-      sim.step();
-      ++step;
-      pf::SystemTotals current = pf::compute_totals(sim.get_bodies());
+      for (int i{}; i < SPF; ++i) {
+        sim.step();
+        ++step;
+        pf::SystemTotals current = pf::compute_totals(sim.get_bodies());
 
-      pf::print_if_not_conserved('E', initial.E, current.E, step);
-      pf::print_if_not_conserved('P', initial.P, current.P, step);
-      pf::print_if_not_conserved('L', initial.L, current.L, step);
+        pf::print_if_not_conserved('E', initial.E, current.E, step);
+        pf::print_if_not_conserved('P', initial.P, current.P, step);
+        pf::print_if_not_conserved('L', initial.L, current.L, step);
+      }
     }
+  } catch (std::exception const &e) {
+    std::cerr << "Standard exception caught: " << e.what() << '\n';
+    return EXIT_FAILURE;
+  } catch (...) {
+    std::cerr << "Unknown exception caught.\n";
+    return EXIT_FAILURE;
   }
 }
